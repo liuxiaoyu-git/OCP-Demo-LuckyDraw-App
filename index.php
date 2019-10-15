655a3ff05d392fd8a29a194036d2a82a83c631ca
liuxiaoyu-git <xiaoyliu@redhat.com>
2019-10-15 20:51:04 +0800

    Update index.php

--- index.php
+++ index.php
@@ -2,4 +2,11 @@
     <tr>
         <td align="center"><font size="6" color="#FF0000">欢迎参加Red Hat会议</font></td>
     </tr>
+    <tr>
+        <td>
+            <?php
+            rand(1,100);
+            ?>
+        </td>
+    </tr>
 </table>
