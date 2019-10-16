7d942cdb33e962f318df8728a224a178d426b204
liuxiaoyu-git <xiaoyliu@redhat.com>
2019-10-16 10:48:53 +0800

    Update index.php

--- index.php
+++ index.php
@@ -9,7 +9,12 @@
         <td align="center">
             <font size="10" color="#FF00FF">
             <?php
-            echo rand(10,99);
+                $result=rand(10,99);
+                echo $result;
+                if ($result=11 || $result=22 || $result=33 || $result=44 || $result=55 || $result=66 || $result=77 || $result=88 || $result=99)
+                    echo "恭喜你，中奖了！";
+                else
+                    echo "这没中奖，还可以再来一次！";
             ?>
             </font>
         </td>
