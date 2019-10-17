40b9170f77f8c8bb8870d65acdfb4da6eb14daf7
liuxiaoyu-git <xiaoyliu@redhat.com>
2019-10-17 08:34:02 +0800

    Update index.php

--- index.php
+++ index.php
@@ -10,31 +10,44 @@ echo "<br>currentLoop2=".$_SESSION["currentLoop"];
 //$_SESSION["currentLoop"] = 1;
 //$_SESSION["win"] = false;
 ?>
-<table width=100%>
-    <tr>
-        <td align="center"><font size="6" color="#FF0000">欢迎参加Red Hat会议</font></td>
-    </tr>
-    <tr>
-        <td align="center"><font size="10" color="#FFFF00">您的抽奖序号是</td>
-    </tr>
-    <tr>
-        <td align="center">
-            <font size="10" color="#FF00FF">
-            <?php
-                $result=rand(10,99);
-                echo $result;
-            ?>
-            </font>
-        </td>
-    </tr>
-    <tr>
-        <td align="center"><font size="10" color="#FF0000">
-            <?php
-                if ($result==11 || $result==22 || $result==33 || $result==44 || $result==55 || $result==66 || $result==77 || $result==88 || $result==99)
-                    echo "恭喜你，中奖了！";
-                else
-                    echo "这没中奖，还可以再来一次！";
-            ?>
-        </td>
-    </tr>
-</table>
+<html>
+    <head>
+        <?php
+            if ($currentLoop == 10)
+                echo "<meta http-equiv=\"refresh\" content=\"2\">";
+        ?>
+    </head>
+    <body>
+        <table width=100%>
+            <tr>
+                <td align="center"><font size="6" color="#FF0000">欢迎参加Red Hat会议</font></td>
+            </tr>
+            <tr>
+                <td align="center"><font size="6" color="#FF0000">这是您的<?=$currentLoo?>第抽奖</font></td>
+            </tr>
+            <tr>
+                <td align="center"><font size="10" color="#FFFF00">您的抽奖序号是</td>
+            </tr>
+            <tr>
+                <td align="center">
+                    <font size="10" color="#FF00FF">
+                    <?php
+                        $result=rand(10,99);
+                        echo $result;
+                    ?>
+                    </font>
+                </td>
+            </tr>
+            <tr>
+                <td align="center"><font size="10" color="#FF0000">
+                    <?php
+                        if ($result==11 || $result==22 || $result==33 || $result==44 || $result==55 || $result==66 || $result==77 || $result==88 || $result==99)
+                            echo "恭喜你，中奖了！";
+                        else
+                            echo "这没中奖，还可以再来一次！";
+                    ?>
+                </td>
+            </tr>
+        </table>
+    </body>
+<html>
