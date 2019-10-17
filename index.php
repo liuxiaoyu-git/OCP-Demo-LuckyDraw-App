80e5e8220132db97ec4d30e55f614f19f6e1b942
liuxiaoyu-git <xiaoyliu@redhat.com>
2019-10-17 11:17:47 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,11 @@
 <html>
-    <body>
-        <table width=100%>
-            <tr>
-                <td align="center"><a href="lucky-draw.php"><font size="50" color="#FF1100">去抽奖</fone></a></td>
-            </tr>
-        </table>
-    </body>
-<html>
+<div id="box">
+    <table width="100%" height="100%">
+        <tr>
+            <td align="center">
+                <a href="lucky-draw.php"><img src="lucky-draw.png" /></a>
+            </td>
+        </tr>
+    </table>
+</div>
+</html>
