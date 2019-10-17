4ea477afd194cbc71f1a5c1d1d71b95de4d63006
liuxiaoyu-git <xiaoyliu@redhat.com>
2019-10-17 10:20:26 +0800

    Update index.php

--- index.php
+++ index.php
@@ -15,6 +15,7 @@ if (session_status() === 1) echo "<br>9=session_status() === 1";
 if (session_status() !== PHP_SESSION_ACTIVE) {
     //echo "<br>start session...";
     session_start();
+    $_SESSION["winTimes"]=(int) $_SESSION["winTimes"];
 }
 
 //echo "<br>currentLoop1=".$_SESSION["currentLoop"];
@@ -32,16 +33,16 @@ $_SESSION["currentLoop"]=$currentLoop;
     <body>
         <table width=100%>
             <tr>
-                <td align="center"><font size="16" color="#FF0000"><b>欢迎参加Red Hat会议</b></font></td>
+                <td align="center"><font size="16" color="#FF0000"><b><br>欢迎参加Red Hat会议</b></font></td>
             </tr>
             <?php
                 if ($currentLoop <=10) {
             ?>
             <tr>
-                <td align="center"><font size="16" color="#FF0000">这是您的第<?=$currentLoop?>抽奖，还可以再抽<?=(10-$currentLoop)?>次</font></td>
+                <td align="center"><font size="16" color="#FF0000"><br>这是您的第<?=$currentLoop?>抽奖，还可以再抽<?=(10-$currentLoop)?>次</font></td>
             </tr>
             <tr>
-                <td align="center"><font size="20" color="#FF0000">本次您的抽奖序号是</td>
+                <td align="center"><font size="20" color="#FF0000"><br>本次您的抽奖序号是</td>
             </tr>
             <tr>
                 <td align="center">
@@ -55,7 +56,7 @@ $_SESSION["currentLoop"]=$currentLoop;
             </tr>
             <tr>
                 <td align="center">
-                    <font size="20" color="#FF0000"><b>
+                    <font size="20" color="#FF0000"><b><br>
                         <?php
                             if ($result==11 || $result==22 || $result==33 || $result==44 || $result==55 || $result==66 || $result==77 || $result==88 || $result==99) {
                                 echo "恭喜你，刚刚中奖了！";
@@ -71,7 +72,7 @@ $_SESSION["currentLoop"]=$currentLoop;
             ?>
             <tr>
                 <td align="center">
-                    <font size="20" color="#FF11FF">你总共中了<br><?=$_SESSION["winTimes"]?></b>奖！</font>
+                    <font size="20" color="#FF11FF"><br>你总共中了<b><?=$_SESSION["winTimes"]?></b>奖！</font>
                 </td>
             </tr>            
             <?php
