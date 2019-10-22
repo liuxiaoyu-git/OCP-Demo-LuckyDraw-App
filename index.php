23e730c612636c049c44873ef6f00a215cd24372
liuxiaoyu-git <xiaoyliu@redhat.com>
2019-10-22 12:44:25 +0800

    Update index.php

--- index.php
+++ index.php
@@ -19,14 +19,14 @@
                 </td>
             </tr>
             <tr>
-                <td align="center">
+                <td align="center"><font size="20" color="#FF00FF">
                     <?php
                         if ($result==11 || $result==22 || $result==33 || $result==44 || $result==55 || $result==66 || $result==77 || $result==88 || $result==99) {
                             echo "<b>恭喜你，刚刚中奖了！</b>";
                         } else
                             echo "<b><br>这没中奖，还可以再来一次！</b>";
                     ?>
-                </td>
+                </font></td>
             </tr>
         </table>
         </div>
