866afa868f46c9de45c921baa079204cc6165167
liuxiaoyu-git <xiaoyliu@redhat.com>
2019-10-22 14:28:05 +0800

    Update lucky-draw-v2.php

--- lucky-draw-v2.php
+++ lucky-draw-v2.php
@@ -36,15 +36,15 @@ $currentLoop=(int) $_SESSION["currentLoop"] + 1;
                 </td>
             </tr>
             <tr>
-                <td align="center">
+                <td align="center"><font size="20" color="#FF1111"><b>
                     <?php
                         if ($result==11 || $result==22 || $result==33 || $result==44 || $result==55 || $result==66 || $result==77 || $result==88 || $result==99) {
-                            echo "<img src=\"smile.png\"><font size=\"20\" color=\"#FF1111\"><b><br>恭喜你，刚刚中奖了！</b></font>";
+                            echo "<img src=\"smile.png\">恭喜你，刚刚中奖了！";
                             $_SESSION["winTimes"] = (int) $_SESSION["winTimes"] + 1;
                         } else
-                            echo "<img src=\"cry.png\"><font size=\"20\" color=\"#FF1111\"><b><br>这没中奖，还可以再来一次！</b></font>";
+                            echo "<img src=\"cry.png\">这没中奖，还可以再来一次！";
                     ?>
-                </td>
+                </b></font></td>
             </tr>
             <?php
                 } else {
