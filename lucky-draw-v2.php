f1b6b74366697242c78d760def430eb1594f2f7d
liuxiaoyu-git <xiaoyliu@redhat.com>
2019-10-22 14:14:42 +0800

    Update lucky-draw-v2.php

--- lucky-draw-v2.php
+++ lucky-draw-v2.php
@@ -6,8 +6,6 @@ if (session_status() !== PHP_SESSION_ACTIVE) {
     $_SESSION["winTimes"]=(int) $_SESSION["winTimes"];
 }
 $currentLoop=(int) $_SESSION["currentLoop"] + 1;
-echo "$currentLoop=";
-echo $currentLoop;
 ?>
 <html>
     <head>
