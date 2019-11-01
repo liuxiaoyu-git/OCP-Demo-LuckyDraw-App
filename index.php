13bd70c910ca638162ba7a68784ba9e688ffcd7e
liuxiaoyu-git <xiaoyliu@redhat.com>
2019-11-01 21:12:34 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,34 +1,29 @@
 <html>
     <body>
-        <div id="box">
         <table width="100%" height="100%">
             <tr>
-                <td align="center"><font size="16" color="#FF0000"><b>欢迎参加Red Hat会议</b></font></td>
-            </tr>
-            <tr>
-                <td align="center"><font size="20" color="#FF1100">本次您的抽奖序号是</td>
-            </tr>
-            <tr>
                 <td align="center">
-                    <font size="20" color="#FF00FF">
-                    <?php
-                        $result=rand(10,99);
-                        echo $result;
-                    ?>
-                    </font>
-                </td>
-            </tr>
-            <tr>
-                <td align="center"><font size="20" color="#FF00FF">
-                    <?php
-                        if ($result==11 || $result==22 || $result==33 || $result==44 || $result==55 || $result==66 || $result==77 || $result==88 || $result==99) {
-                            echo "<b>恭喜你，刚刚中奖了！</b>";
-                        } else
-                            echo "<b><br>这次没中奖，可以再扫一次码,重新抽奖！</b>";
-                    ?>
-                </font></td>
-            </tr>
+					<a href="luckydraw.php">
+					1）去抽奖
+					</a>
+					<br/>
+					<a href="showwinner.php">
+					2）显示已中奖用户
+					</a>
+					<br/>
+					<a href="initapp.php">
+					3）重置抽奖应用（清除所有注册抽奖的用户）
+					</a>
+					<br/>
+					<a href="sample.php">
+					4）生成模拟数据
+					</a>
+					<br/>
+					<a href="test.php">
+					5）功能测试
+					</a>
+				</td>
+			</tr>
         </table>
-        </div>
     </body>
 </html>
